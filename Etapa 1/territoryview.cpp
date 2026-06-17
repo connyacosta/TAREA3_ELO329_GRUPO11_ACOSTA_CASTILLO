f262bd4af74c1699f9a90d6a4a1387fc4af663fa
jojita087 <167333327+jojita087@users.noreply.github.com>
2026-06-17 19:55:32 -0400

    Implement TerritoryView class en .cpp

---
+++ Etapa 1/territoryview.cpp
@@ -0,0 +1,35 @@
+#include "territoryview.h"
+
+#include <QGraphicsPixmapItem>
+#include <QPixmap>
+#include <QDebug>
+
+TerritoryView::TerritoryView(const QString& imageFile)
+{
+    scene = new QGraphicsScene(this);
+
+    QPixmap pix(imageFile);
+
+    if (pix.isNull())
+    {
+        qDebug() << "No se pudo cargar la imagen:"
+                 << imageFile;
+    }
+    else
+    {
+        qDebug() << "Imagen cargada:"
+                 << imageFile;
+    }
+
+    scene->addPixmap(pix);
+
+    scene->setSceneRect(pix.rect());
+
+    setScene(scene);
+    setDragMode(QGraphicsView::ScrollHandDrag);
+
+}
+QGraphicsScene* TerritoryView::getScene()
+{
+    return scene;
+}
