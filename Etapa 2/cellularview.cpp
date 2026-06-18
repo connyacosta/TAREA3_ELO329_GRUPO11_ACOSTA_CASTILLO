42ad210b8f1dd2b0a6649820a9367c2e959e04df
jojita087 <167333327+jojita087@users.noreply.github.com>
2026-06-17 22:10:48 -0400

    Update cellularview.cpp con movimiento

---
+++ Etapa 2/cellularview.cpp
@@ -0,0 +1,38 @@
+#include "cellularview.h"
+
+#include <QGraphicsRectItem>
+#include <QGraphicsTextItem>
+#include <QBrush>
+
+CellularView::CellularView(Cellular *cellular):cellular(cellular)
+{
+    double width = 12;
+    double height = 24;
+
+    auto* rect =
+        new QGraphicsRectItem(-width/2,
+                              -height/2,
+                              width,
+                              height);
+
+    rect->setBrush(QBrush(Qt::blue));
+
+    auto* label =
+        new QGraphicsTextItem(cellular->getOwnerName());
+
+    label->setPos(width/2 + 4, -5);
+    label->setDefaultTextColor(Qt::blue);
+
+    addToGroup(rect);
+    addToGroup(label);
+
+    setPos(cellular->getX(),cellular->getY());
+
+    updatePosition();
+
+}
+void CellularView::updatePosition()
+{
+    setPos(cellular->getX(),
+           cellular->getY());
+}
