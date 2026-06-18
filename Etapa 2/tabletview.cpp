d833b42919ffb01b7c0219d5fa8578ff18997458
jojita087 <167333327+jojita087@users.noreply.github.com>
2026-06-17 22:26:07 -0400

    Implement TabletView class

---
+++ Etapa 2/tabletview.cpp
@@ -0,0 +1,36 @@
+#include "tabletview.h"
+#include <QBrush>
+#include <QGraphicsRectItem>
+#include <QGraphicsTextItem>
+
+TabletView::TabletView(Tablet *tablet)
+    : tablet(tablet)
+{
+    auto* rect =
+        new QGraphicsRectItem(
+            -10,
+            -7,
+            20,
+            14);
+
+    rect->setBrush(QColor(Qt::green));
+
+    auto* label =
+        new QGraphicsTextItem(
+            "Tablet\n"
+            + tablet->getOwnerName());
+
+    label->setPos(14,-5);
+    label->setDefaultTextColor(Qt::green);
+
+    addToGroup(rect);
+    addToGroup(label);
+
+    updatePosition();
+}
+
+void TabletView::updatePosition()
+{
+    setPos(tablet->getX(),
+           tablet->getY());
+}
