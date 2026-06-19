f102feb99004f03ef8f81d6a5635350779f66995
jojita087 <167333327+jojita087@users.noreply.github.com>
2026-06-19 18:58:36 -0400

    implementación territoryview

---
+++ Etapa 3/territoryview.cpp
@@ -0,0 +1,45 @@
+#include "territoryview.h"
+
+#include <QGraphicsPixmapItem>
+#include <QPixmap>
+#include <QDebug>
+
+TerritoryView::TerritoryView(const QString& imageFile)
+{
+    scene = new QGraphicsScene(this);
+
+    QPixmap pix(imageFile);
+
+    if (pix.isNull())
+    {
+        qDebug() << "No se pudo cargar la imagen:"
+                 << imageFile;
+    }
+    else
+    {
+        qDebug() << "Imagen cargada:"
+                 << imageFile;
+    }
+
+    scene->addPixmap(pix);
+
+    scene->setSceneRect(pix.rect());
+
+    setScene(scene);
+    setDragMode(QGraphicsView::ScrollHandDrag);
+
+}
+QGraphicsScene* TerritoryView::getScene()
+{
+    return scene;
+}
+
+double TerritoryView::getWidth() const
+{
+    return scene->sceneRect().width();
+}
+
+double TerritoryView::getHeight() const
+{
+    return scene->sceneRect().height();
+}
