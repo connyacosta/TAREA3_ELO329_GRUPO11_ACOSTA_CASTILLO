5e08ef92b65e86294dff3dc0bcf9522387bc1daf
jojita087 <167333327+jojita087@users.noreply.github.com>
2026-06-19 18:31:49 -0400

    clase tabletview.h

---
+++ Etapa 3/tabletview.h
@@ -0,0 +1,19 @@
+#ifndef TABLETVIEW_H
+#define TABLETVIEW_H
+
+#include <QGraphicsItemGroup>
+
+#include "tablet.h"
+
+class TabletView : public QGraphicsItemGroup
+{
+private:
+    Tablet* tablet;
+
+public:
+    TabletView(Tablet* tablet);
+
+    void updatePosition();
+};
+
+#endif
